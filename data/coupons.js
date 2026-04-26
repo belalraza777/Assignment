2fc2fd57c93ff4b3886c2580fe2eb3acdf4371a1
belal raza <belalraza707@gmail.com>
2026-04-26 10:46:41 +0530

    #

--- data/coupons.js
+++
@@ -1,5 +0,0 @@
-// Simple in-memory array to store coupons.
-// This acts like a "fake database" for the assignment.
-let coupons = [];
-
-module.exports = coupons;
--- data/usageTracker.js
+++
@@ -1,5 +0,0 @@
-// Tracks how many times each user has used each coupon.
-// Format:  usageTracker["userId|couponCode"] = count
-let usageTracker = {}; 
-
-module.exports = usageTracker;
